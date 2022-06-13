5403c7570fe0a3d2b17d182fe3a83eddb82367ff
0gis0 <giselatb@outlook.com>
2022-06-13 16:27:41 +0200

    refactor model

--- app.py
+++ app.py
@@ -1,59 +1,58 @@
 from flask import Flask, render_template, request, abort
-from model import db, get_hero, remove_hero, get_hero_by_name, addHero, updateHero, messages
+from model import HeroService
 
 app = Flask(__name__)
+heroService = HeroService()
 
 
 @app.route('/')
 def index():
-    heroes = db[1:5]
-    return render_template('index.html', heroes=heroes, messages=messages)
+    return render_template('index.html', heroes=heroService.get_all_heroes(), messages=heroService.messages)
 
 
 @app.route('/heroes')
 def heroes():
-    return render_template('heroes.html', heroes=db[1:10], messages=messages)
+    return render_template('heroes.html', heroes=heroService.get_all_heroes(), messages=heroService.messages)
 
 
 @app.route('/hero/<int:id>')
 def hero_detail(id):
-    hero = get_hero(id)
-    return render_template('detail.html', hero=hero, messages=messages)
+    return render_template('detail.html', hero=heroService.get_hero(id), messages=heroService.messages)
 
 
 @app.route('/hero', methods=['POST'])
 def add_hero():
     name = request.form.get('new_hero')
-    addHero(name)
-    return render_template('heroes.html', heroes=db[1:10], messages=messages)
+    heroService.addHero(name)
+    return render_template('heroes.html', heroes=heroService.get_all_heroes(), messages=heroService.messages)
 
 
 @app.route('/hero/<int:id>', methods=['POST'])
 def update_hero(id):
-    hero = get_hero(id)
+    hero = heroService.get_hero(id)
     hero['name'] = request.form.get('hero_name')
     hero['alterEgo'] = request.form.get('alterEgo')
     hero['description'] = request.form.get('description')
 
-    updateHero(hero)
-    return render_template('heroes.html',  heroes=db[1:10], messages=messages)
+    heroService.updateHero(hero)
+    return render_template('heroes.html',  heroes=heroService.get_all_heroes(), messages=heroService.messages)
 
 
 @app.route('/delete/<int:id>')
 def delete_hero(id):
-    remove_hero(id)
-    return render_template('heroes.html', heroes=db[1:10], messages=messages)
+    heroService.remove_hero(id)
+    return render_template('heroes.html', heroes=heroService.get_all_heroes(), messages=heroService.messages)
 
 
 @app.route('/search')
 def search():
-    hero = get_hero_by_name(request.args.get('query'))
+    hero = heroService.get_hero_by_name(request.args.get('query'))
     if hero:
-        return render_template('detail.html', hero=hero, messages=messages)
+        return render_template('detail.html', hero=hero, messages=heroService.messages)
     else:
         abort(404)
 
 @app.route('/clear_messages')
 def clear_messages():
-    messages.clear()
-    return render_template('heroes.html', heroes=db[1:10], messages=messages)
+    heroService.messages.clear()
+    return render_template('heroes.html', heroes=heroService.get_all_heroes(), messages=heroService.messages)
--- model.py
+++ model.py
@@ -1,6 +1,57 @@
 import json
 
 
+class HeroService:
+    def __init__(self):
+        self.db = load_db()
+        self.messages = []
+
+    def get_all_heroes(self):
+        return self.db[1:10]
+
+    def get_hero(self, id):
+        for hero in self.db:
+            if hero['id'] == id:
+                self.messages.append(f'Hero with id {id} found')
+                return hero
+        return None
+
+    def get_hero_by_name(self, query):
+        for hero in self.db:
+            if hero['name'].lower() == query.lower():
+                self.messages.append(f'Hero with name {query} found')
+                return hero
+        return None
+
+    def addHero(self, name):
+        hero = {'id': len(self.db) + 1, 'name': name,
+                'description': '', 'alterEgo': ''}
+        self.db.append(hero)
+        save_db(self.db)
+        self.messages.append(f'Hero with name {name} added')
+
+    def updateHero(self, hero):
+        index = 0
+        for h in self.db:
+            if h['id'] == hero['id']:
+                self.db[index] = hero
+                save_db(self.db)
+                self.messages.append(f"Hero with id {hero['id']} updated")
+                return hero
+            index = index + 1
+        return None
+
+    def remove_hero(self, id):
+        index = 0
+        for hero in self.db:
+            if hero['id'] == id:
+                del self.db[index]
+                save_db(self.db)
+                self.messages.append(f"Hero with id {id} deleted")
+                return hero
+            index = index + 1
+
+
 def load_db():
     with open('heroes_db.json', 'r') as f:
         return json.load(f)
@@ -11,48 +62,49 @@ def save_db(db):
         json.dump(db, f)
 
 
-def get_hero(id):
-    for hero in db:
-        if hero['id'] == id:
-            messages.append(f'Hero with id {id} found')
-            return hero
-    return None
+# def get_hero(id):
+#     for hero in db:
+#         if hero['id'] == id:
+#             messages.append(f'Hero with id {id} found')
+#             return hero
+#     return None
 
-def get_hero_by_name(query):
-    for hero in db:
-        if hero['name'].lower() == query.lower():
-            messages.append(f'Hero with name {query} found')
-            return hero
-    return None
+# def get_hero_by_name(query):
+#     for hero in db:
+#         if hero['name'].lower() == query.lower():
+#             messages.append(f'Hero with name {query} found')
+#             return hero
+#     return None
 
-def addHero(name):
-    hero = {'id': len(db) + 1, 'name': name, 'description': '', 'alterEgo': ''}
-    db.append(hero)
-    save_db(db)
-    messages.append(f'Hero with name {name} added')
+# def addHero(name):
+#     hero = {'id': len(db) + 1, 'name': name, 'description': '', 'alterEgo': ''}
+#     db.append(hero)
+#     save_db(db)
+#     messages.append(f'Hero with name {name} added')
 
 
-def updateHero(hero):
-    index = 0
-    for h in db:
-        if h['id'] == hero['id']:
-            db[index] = hero
-            save_db(db)
-            messages.append(f"Hero with id {hero['id']} updated")
-            return hero
-        index = index + 1
-    return None
+# def updateHero(hero):
+#     index = 0
+#     for h in db:
+#         if h['id'] == hero['id']:
+#             db[index] = hero
+#             save_db(db)
+#             messages.append(f"Hero with id {hero['id']} updated")
+#             return hero
+#         index = index + 1
+#     return None
 
-def remove_hero(id):
-    index = 0
-    for hero in db:
-        if hero['id'] == id:
-            del db[index]
-            save_db(db)
-            messages.append(f"Hero with id {id} deleted")
-            return hero
-        index = index + 1
 
-db = load_db()
+# def remove_hero(id):
+#     index = 0
+#     for hero in db:
+#         if hero['id'] == id:
+#             del db[index]
+#             save_db(db)
+#             messages.append(f"Hero with id {id} deleted")
+#             return hero
+#         index = index + 1
 
-messages = []
+# db = load_db()
+
+# messages = []
